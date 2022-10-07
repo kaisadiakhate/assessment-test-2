dd2a2949b5404c805532ef5589d331363bb10998
kaisadiakhate <kaisa.diakhate@awacademy.training>
2022-10-07 09:43:03 +0300

    added total and average functions

--- frontend/src/shoppingCartUtils.js
+++ frontend/src/shoppingCartUtils.js
@@ -1,15 +1,25 @@
 // Takes a list of fruit objects as arguments
 // eg. [{'name': 'Banana', 'price': 1}, {'name': 'Apple', 'price': 2}]
 // Should return the cart total.
-function calculateTotal (items) {
-  return 9999;
+function calculateTotal(items) {
+  let total = 0;
+  for (let i = 0; i < items.length; i++) {
+    total += items[i].price;
+  }
+  return total;
 }
 
 // Takes a list of fruit objects as arguments
 // eg. [{'name': 'Banana', 'price': 1}, {'name': 'Apple', 'price': 2}]
 // Should return the cart average.
-function calculateAvg (items) {
-  return 9999;
+function calculateAvg(items) {
+  let average = 0;
+  let total = 0;
+  for (let i = 0; i < items.length; i++) {
+    total += items[i].price;
+    average = total / items.length;
+  }
+  return average;
 }
 
 export { calculateTotal, calculateAvg };
